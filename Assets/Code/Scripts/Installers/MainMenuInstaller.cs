fd0e15c1f2ad1ce12e57ee7a561f4366fadbccf7
i-ka <i-ka2010@mail.ru>
2024-05-09 16:44:54 +0500

    Add settings screen

--- Assets/Code/Scripts/Installers/MainMenuInstaller.cs
+++ Assets/Code/Scripts/Installers/MainMenuInstaller.cs
@@ -6,16 +6,20 @@ using Zenject;
 public class MainMenuInstaller : MonoInstaller
 {
     [SerializeField] private MainMenuView mainMenu;
-
-    [SerializeField] private AudioListener audioListener;
+    [SerializeField] private SettingsMenuView settingsMenu;
     
     public override void InstallBindings()
     {
         Container.Bind<MainMenuController>().FromNew().AsSingle();
         Container.Bind<SettingsController>().FromNew().AsSingle();
         
+        var settingsMenuInstance = Container.InstantiatePrefabForComponent<SettingsMenuView>(settingsMenu);
+        Container.Bind<SettingsMenuView>().FromInstance(settingsMenuInstance).AsSingle();
+        settingsMenuInstance.gameObject.SetActive(false);
+        
         var mainMenuInstance = Container.InstantiatePrefabForComponent<MainMenuView>(mainMenu);
         Container.Bind<MainMenuView>().FromInstance(mainMenuInstance).AsSingle();
         
+
     }
 }
