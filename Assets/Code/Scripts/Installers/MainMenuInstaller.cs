44d72b0e32c85314eda1c833eb3898f7e26a0ba5
i-ka <i-ka2010@mail.ru>
2024-05-09 15:58:13 +0500

    Add settings controller

--- Assets/Code/Scripts/Installers/MainMenuInstaller.cs
+++ Assets/Code/Scripts/Installers/MainMenuInstaller.cs
@@ -1,14 +1,18 @@
 using Code.Scripts.MainMenu;
 using UnityEngine;
+using UnityEngine.Rendering;
 using Zenject;
 
 public class MainMenuInstaller : MonoInstaller
 {
     [SerializeField] private MainMenuView mainMenu;
+
+    [SerializeField] private AudioListener audioListener;
     
     public override void InstallBindings()
     {
         Container.Bind<MainMenuController>().FromNew().AsSingle();
+        Container.Bind<SettingsController>().FromNew().AsSingle();
         
         var mainMenuInstance = Container.InstantiatePrefabForComponent<MainMenuView>(mainMenu);
         Container.Bind<MainMenuView>().FromInstance(mainMenuInstance).AsSingle();
