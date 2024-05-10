89b1f7ce4342621c808aef17b1ffd953767a796d
SubbotaHub <vlad.subbotin.06@mail.ru>
2024-05-10 19:00:46 +0300

    Add VFX

--- Assets/Code/Scripts/Weapon/Bullet.cs
+++ Assets/Code/Scripts/Weapon/Bullet.cs
@@ -1,20 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 [RequireComponent(typeof(Rigidbody))]
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _maxDamage;
+    public VisualEffect _blast;
 
-    [SerializeField] private GameObject _blast;
+    private Blast _blastLogic;
+    private Rigidbody _rigidbody;
     private bool _exploded;
 
-    private void Update()
+    private void Start()
     {
-        transform.Translate(transform.up * _speed * Time.deltaTime, transform);
+        _rigidbody = GetComponent<Rigidbody>();
+        _blastLogic = GetComponent<Blast>();
+        _rigidbody.velocity = -transform.forward * _speed;
     }
+
+
     private void OnCollisionEnter(Collision coll)
     {
         if (coll.gameObject.CompareTag("Enemy"))
@@ -22,11 +29,13 @@ public class Bullet : MonoBehaviour
             //получение макс урона врагом
         }
 
-        if (!_exploded)
+        if (!_exploded && !coll.gameObject.CompareTag("Player"))
         {
-            Instantiate(_blast, transform.position + transform.right, Quaternion.identity);
+            //Instantiate(_blast, transform.position + -transform.forward, Quaternion.identity);
             _exploded = true;
+            _blast.SendEvent("OnHit");
+            _rigidbody.velocity = new Vector3(0, 0, 0);
+            _blastLogic.Explode();
         }
-        Destroy(this.gameObject);
     }
 }
